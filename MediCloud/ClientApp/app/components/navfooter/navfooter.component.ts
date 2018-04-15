9ec218cc4980078084df1b74fbab12677e51567d
Mohammed TAFISH <honeyize14@hotmail.com>
2018-04-15 11:41:56 +0300

    main page

---
+++ MediCloud/ClientApp/app/components/navfooter/navfooter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nav-footer',
+    templateUrl: './navfooter.component.html',
+    styleUrls: ['./navfooter.component.css']
+})
+export class NavFooterComponent {
+}
